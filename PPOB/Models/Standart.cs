a1bb626512b6d6c39af2ad0a39dd34b397f393bf
nanasupiana <34092138+nanasupiana@users.noreply.github.com>
2018-04-01 14:22:35 +0700

    New Repository PPOB2
    New Repository PPOB2

---
+++ PPOB/Models/Standart.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+
+namespace PPOB.Models
+{
+
+    public class StandartComboBox
+    {
+        public string Value { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class TransactionHelper
+    {
+        SqlCommand _Cmd = new SqlCommand();
+        public SqlCommand Command
+        {
+            get { return _Cmd; }
+            set { _Cmd = value; }
+        }
+    }
+}
