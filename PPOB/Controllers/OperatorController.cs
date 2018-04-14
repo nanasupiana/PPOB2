4ed8881f6f8b72873ca73c6a2bd4b327618f2e58
dedi007 <37971600+dedi007@users.noreply.github.com>
2018-04-14 14:37:33 +0700

    Add Library

---
+++ PPOB/Controllers/OperatorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using PPOB.Models.Operator;
+
+namespace PPOB.Controllers
+{
+    public class OperatorController : Controller
+    {
+        // GET: Operator
+        public async Task<ActionResult> Operator()
+        {
+            await GetOperator();
+            return View();
+        }
+
+        public async Task<ActionResult> GetOperator()
+        {
+            ViewData["ListOeraator"] = await OperatorRepository.GetOperator();
+            return PartialView("_SimpleGrid");
+        }
+    }
+}
---
+++ PPOB/Models/Operator/OperatorRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using mmt_Library;
+namespace PPOB.Models.Operator
+{
+    public class OperatorRepository
+    {
+        dbAccess dbAccess = new dbAccess();
+
+
+    }
+}
