156b16f2aeaaef07831e176d6d7f77c73807608d
Nana Supiana <nn.supiana@gmail.com>
2018-04-29 14:14:45 -0700

    Type Member dan Pulsa

---
+++ PPOB/Models/Menu/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PPOB.Models.Menu
+{
+    public class Menu
+    {
+        public int MID;
+        public string MenuName;
+        public string MenuURL;
+        public int MenuParentID;
+    }
+}
---
+++ PPOB/Models/Pulsa/MasterPulsa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PPOB.Models.Pulsa
+{
+    public class MasterPulsa
+    {
+        public string PulsaId { get; set; }
+        public string Deskripsi { get; set; }
+    }
+}
